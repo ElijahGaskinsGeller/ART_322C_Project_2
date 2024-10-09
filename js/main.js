4f5e425830b4f342c8144964a388ac7460a30a3b
Elijah Jeruh <elijahgaskinsgeller@gmail.com>
2024-10-09 10:49:40 -0700

    started project. cube map, scroll, falling leafs

---
+++ js/main.js
@@ -0,0 +1,206 @@
+
+
+import * as THREE from 'three';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
+
+
+function GetPageHeight() {
+	return Math.max(document.body.scrollHeight, document.body.offsetHeight,
+		document.documentElement.clientHeight, document.documentElement.scrollHeight,
+		document.documentElement.offsetHeight);
+}
+
+function WindowScrollNormalPosition() {
+	return window.scrollY / (GetPageHeight() - window.innerHeight);
+}
+
+function lerp(start, end, amt) {
+	return (1 - amt) * start + amt * end
+}
+
+function randomRange(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function randomRangeInt(min, max) {
+	return Math.round(randomRange(min, max));
+}
+
+function setLeafPos(leaf) {
+
+	leaf.position.x = randomRange(-1000, 1000);
+	leaf.position.y = 1000;
+	//leaf.position.y = randomRange(-1000, 1000);
+	leaf.position.z = randomRange(-100, 300);
+
+
+	//leaf.scale.x = leaf.scale.y = leaf.scale.z = randomRange(.75, 1.75);
+}
+
+console.log(THREE);
+
+
+let scene = new THREE.Scene();
+
+let environment = new THREE.CubeTextureLoader()
+	.setPath("./imgs/test/")
+	.load([
+		'left.jpg',
+		'right.jpg',
+		'top.jpg',
+		'bottom.jpg',
+		'back.jpg',
+		'front.jpg',
+	]);
+
+
+scene.background = environment;
+
+
+let camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 2000);
+
+camera.position.z = 5;
+
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+let manager = new THREE.LoadingManager();
+let loader = new FBXLoader(manager);
+
+//let leafMesh = new THREE.BoxGeometry(10, 2, 5);
+let leafs = [];
+let leafMesh = null;
+let leafCount = 500;
+let leafColorRange = [
+	0xFBBF07,
+	0xF49004,
+	0xE35704,
+	0xA92A0A,
+	0xF8B005
+];
+
+
+loader.load("./models/leaf_0.fbx", function(group) {
+
+	console.log(group);
+	leafMesh = group.children[0].geometry;
+
+
+	for (let i = 0; i < leafCount; i++) {
+
+
+		let currentColor = leafColorRange[randomRangeInt(0, leafColorRange.length - 1)];
+		let leafMaterial = new THREE.MeshBasicMaterial({ color: new THREE.Color(currentColor) });
+		leafMaterial.transparent = true;
+		let newLeaf = group.children[0].clone(true);
+		newLeaf.material = leafMaterial;
+
+		leafs.push(newLeaf);
+
+		scene.add(newLeaf);
+
+		setLeafPos(newLeaf);
+
+		newLeaf.position.y = randomRange(-25, 1000);
+		console.log(newLeaf.position.y)
+
+		let scale = randomRange(.01, .02);
+
+		newLeaf.scale.x *= scale;
+		newLeaf.scale.y *= scale;
+		newLeaf.scale.z *= scale;
+
+		newLeaf.rotation.z = randomRange(0, 2 * Math.PI);
+
+	}
+
+});
+
+
+//let geometry = new THREE.BoxGeometry(1, 1, 1);
+//let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+//let cube = new THREE.Mesh(geometry, material);
+//scene.add(cube);
+
+
+
+
+function OnWindowResize(e) {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function OnScroll(e) {
+	let scrollStart = Math.PI / 2;
+	let scrollEnd = -Math.PI / 2;
+
+	let currentPos = WindowScrollNormalPosition();
+
+
+	let currentRotation = lerp(scrollStart, scrollEnd, currentPos);
+
+	camera.rotation.x = currentRotation;
+
+}
+
+let prevFrameTime = 0;
+
+function animate(time) {
+	//cube.rotation.x += 0.01;
+	//cube.rotation.y += 0.01;
+	//camera.rotation.x = Math.sin(time / 10000);
+
+	let deltaTime = (time - prevFrameTime) / 1000;
+	prevFrameTime = time;
+
+	let fallSpeed = 10;
+	let forwardSpeed = 5;
+	let rotationSpeed = Math.PI / 12;
+	let opacitySpeed = 1;
+	for (let i = 0; i < leafs.length; i++) {
+
+		let currentLeaf = leafs[i];
+
+
+		if (currentLeaf.position.y <= -75) {
+
+			currentLeaf.material.opacity -= (opacitySpeed * deltaTime);
+
+			if (currentLeaf.material.opacity <= 0) {
+
+				currentLeaf.material.opacity = 1;
+				setLeafPos(currentLeaf);
+
+			}
+
+		} else {
+
+			currentLeaf.position.y -= fallSpeed * deltaTime;
+			currentLeaf.position.z -= forwardSpeed * deltaTime;
+
+			currentLeaf.rotation.z += rotationSpeed * deltaTime;
+
+		}
+
+	}
+
+	renderer.render(scene, camera);
+
+
+	requestAnimationFrame(animate);
+}
+
+
+
+window.addEventListener("resize", OnWindowResize);
+window.addEventListener("scroll", OnScroll);
+
+OnScroll();
+animate(0);
